658aee27f55230105ffcb574ce01bef966f6d04a
LAPTOP-7OQGSNPQ\jacr1 <jacr1975manizales@yahoo.com>
2023-07-03 09:46:55 -0500

    first commit

---
+++ src/main/java/com/example/awsebeanstalk/AwsEbeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsebeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEbeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEbeanstalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsebeanstalk/AwsEbeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsebeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEbeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
